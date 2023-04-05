f2d9f680ff4379f735172acc64d452e1281fc719
ShivaPandey02 <shiva.pandey@crowdbotics.com>
2023-04-06 01:02:42 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "aged-water-39620",
-  url: "https://aged-water-39620.botics.co",
-  api: "https://aged-water-39620.botics.co/api/v1"
-}
+  "name": "aged-water-39620",
+  "url": "https://aged-water-39620.botics.co",
+  "api": "https://aged-water-39620.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
